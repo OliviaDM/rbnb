5e063d55c56d4351270d6aa9d048661844a015c0
Olivia De Meyere <oliviademeyere@gmail.com>
2019-11-04 08:09:20 -0800

    basic but no routes

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,37 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: [:show, :edit, :update, :destroy]
+
+  def show
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.save
+  end
+
+  def edit
+  end
+
+  def update
+    @booking.update(booking_params)
+    @booking.save
+  end
+
+  def destroy
+    @booking.destroy
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:sucker_id, :beast_id, :date_start, :date_end)
+  end
 end
