1a94d41e7836d6d35f6aa526476b0eb891b342ef
Nicola Tournay <nicolatournay@outlook.com>
2019-11-04 17:31:47 +0100

    BeastsController created + relevant routes

---
+++ app/controllers/beasts_controller.rb
@@ -0,0 +1,44 @@
+class BeastsController < ApplicationController
+  before_action :set_current_beast, only: [:show, :edit, :update, :destroy, :chef]
+  def index
+    @beasts = Beast.all
+  end
+
+  def show
+  end
+
+  def new
+    @beast = Beast.new(beast_params)
+  end
+
+  def create
+    @beast = Beast.new(beast_params)
+    @beast.save
+    redirect_to @beast, notice: 'Beast was successfully created.'
+  end
+
+  def edit
+  end
+
+  def update
+    if @current_beast.update(beast_params)
+      redirect_to @beast, notice: 'Beast was successfully updated.'
+    else
+      render :edit
+  end
+
+  def destroy
+    @current_beast.destroy
+    redirect_to beasts_url, notice: 'Beast was successfully destroyed.'
+  end
+
+  private
+
+  def set_current_beast
+    @current_beast = Beast.find(params[:id])
+  end
+
+  def beast_params
+    params.require(:beast).permit(:sucker, :type, :name, :region, :description)
+  end
+end
