ce720c88848c491dddc4d9552aa9a9dbd98371cc
Olivia De Meyere <oliviademeyere@gmail.com>
2019-11-05 02:22:23 -0800

    inintial seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,52 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+user = Sucker.new(name: "Quarantaine", user_name: "loser", email: "loseremail@loser.loser", phone: "+123", password: "pregnfnsat", avatar: "beanface.png")
+user.save
+
+user = Sucker.new(name: "Olivia", user_name: "liv", email: "coolemail@cool.cool", phone: "pls don't", password: "password", avatar: "cuteface.png")
+user.save
+
+user = Sucker.new(name: "Nicola", user_name: "nicolas", email: "niconicos@email.com", phone: "444719", password: "uggggggh", avatar: "beardface.png")
+user.save
+
+user = Sucker.new(name: "Na-oh-zky", user_name: "nautsky", email: "cjhvdjhbvd@jhchjvd.j", phone: "911", password: "imfabulous69", avatar: "hatface.png")
+user.save
+
+poke = Type.new(name: "pokemon")
+poke.save
+
+10.times do
+  owner = User.all.sample
+  region = Faker::Games::Pokemon.location
+  description = Faker::Games::Pokemon.move
+  name = Faker::Games::Pokemon.name
+  beast = Beast.new(name: name, region: region, description: description, sucker_id: owner.id, type_id: poke.id)
+  beast.save
+end
+
+sonic = Type.new(name: "sonic")
+sonic.save
+
+10.times do
+  owner = User.all.sample
+  region = Faker::Games::SonicTheHedgehog.zone
+  description = Faker::Games::SonicTheHedgehog.game
+  name = Faker::Games::SonicTheHedgehog.character
+  beast = Beast.new(name: name, region: region, description: description, sucker_id: owner.id, type_id: sonic.id)
+  beast.save
+end
+
+zelda = Type.new(name: "zelda")
+zelda.save
+
+10.times do
+  owner = User.all.sample
+  region = Faker::Games::Zelda.location
+  description = Faker::Games::Zelda.game
+  name = Faker::Games::Zelda.character
+  beast = Beast.new(name: name, region: region, description: description, sucker_id: owner.id, type_id: zelda.id)
+  beast.save
+end
