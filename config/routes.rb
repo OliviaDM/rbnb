96de112b29b7bf249f9aaf46b4fff6fc8c3d27a3
Olivia De Meyere <oliviademeyere@gmail.com>
2019-11-05 01:01:35 -0800

    did some routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   resources :beasts
+  resources :booking, only: [:show, :new, :create, :edit, :update, :destroy]
+  resources :sucker, only: [:show, :new, :create, :edit, :update]
+  resources
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
