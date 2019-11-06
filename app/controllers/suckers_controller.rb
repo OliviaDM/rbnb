6c98b415fbd7e4d604f478e7bea1549e1909526e
corentin dawans <dawans.corentin@gmail.com>
2019-11-06 11:26:16 +0100

    setting before_action authentification

--- app/controllers/suckers_controller.rb
+++ app/controllers/suckers_controller.rb
@@ -1,4 +1,5 @@
 class SuckersController < ApplicationController
+  before_action :authenticate_sucker!, only: [:edit, :update]
   before_action :set_current_sucker, only: [:show, :edit, :update]
   def new
     @sucker = Sucker.new()
